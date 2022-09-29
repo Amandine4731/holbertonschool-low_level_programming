f2962a591c2ce1014487993fcebce92cccfbde3e
Amandine4731 <5618@holbertonstudents.com>
2022-09-29 06:20:04 -0700

    learning C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle"); //Prints the sentence on the screen
 return (0);
 }
