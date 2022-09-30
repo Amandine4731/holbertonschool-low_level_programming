ac397f8f38f94a10e049c8cc04013c1e502d8859
Amandine4731 <5618@holbertonstudents.com>
2022-09-30 02:17:33 -0700

    learning C

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %ld bytes\n",sizeof(charType));
-	printf("Size of int: %ld bytes\n",sizeof(integerType));
-	printf("Size of long int: %ld bytes\n",sizeof(integerType));
-	printf("Size of long long int: %ld bytes\n",sizeof(integerType));
-	printf("Size of float: %ld bytes\n",sizeof(floatType));
+	printf("Size of char: %ld bytes\n",sizeof(char));
+	printf("Size of int: %ld bytes\n",sizeof(int));
+	printf("Size of long int: %ld bytes\n",sizeof(int));
+	printf("Size of long long int: %ld bytes\n",sizeof(int));
+	printf("Size of float: %ld bytes\n",sizeof(float));
 	return (0);
 }
