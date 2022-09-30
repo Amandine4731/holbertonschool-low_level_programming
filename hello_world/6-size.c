60b324b67c883879d35342fda3f6c77b8bb25293
Amandine4731 <5618@holbertonstudents.com>
2022-09-30 02:01:20 -0700

    learning C

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %ld bytes\n");
+	printf("Size of int: %ld bytes\n");
+	printf("Size of long int: %ld bytes\n");
+	printf("Size of long long int: %ld bytes\n");
+	printf("Size of float: %ld bytes\n");
+	return (0);
+}
