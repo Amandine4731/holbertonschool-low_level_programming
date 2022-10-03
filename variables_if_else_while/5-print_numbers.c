28d7497b58d911e0f67036f1c3a771d2d7ebfb43
Amandine4731 <5618@holbertonstudents.com>
2022-10-03 07:02:14 -0700

    learning C variables, if, else, while

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch = '0';
-		
+
 	for (ch = '0'; ch <= '9'; ch++)
 	putchar(ch);
 	putchar('\n');
