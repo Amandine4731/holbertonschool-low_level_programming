66e7a25be56ba1a35d530154f871e69c127bfab7
Amandine4731 <5618@holbertonstudents.com>
2022-10-03 04:44:12 -0700

    learning C variables, if, else, while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, der);
 	/* 2nd condition */
 
-	else if (n = 0)
+	else if (n == 0)
 	printf("Last digit of %d is %d and is 0\n", n, der);
 	/* other conditions */
 	else
