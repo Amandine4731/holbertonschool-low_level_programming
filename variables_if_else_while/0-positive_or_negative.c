b8171e40615ece611d3c790a815e1ac6f4a5ea3c
Amandine4731 <5618@holbertonstudents.com>
2022-10-03 02:09:09 -0700

    learning C variables, if, else, while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
-{
+int main(void) {
 	int n;
-		
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("value of n: %d\n", n);
+
+	if (n = 0)
+	printf("%d=0 is zero\n", n);
+
+	else if (n < 0)
+        printf("%d<0 is negative\n", n);
+
+        else
+        printf("%d>0 is positive\n", n);
+
 	return (0);
 }
