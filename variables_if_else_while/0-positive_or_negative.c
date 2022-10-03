2550e7cf9e788d2361ad0922a92336a0d51715e3
Amandine4731 <5618@holbertonstudents.com>
2022-10-03 02:11:45 -0700

    learning C variables, if, else, while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void) {
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n = 0)
+	if(n = 0)
 	printf("%d=0 is zero\n", n);
 
-	else if (n < 0)
+	else if(n < 0)
         printf("%d<0 is negative\n", n);
 
         else
