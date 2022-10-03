dd99e2629077d9b9a9a46a8b705979314e58ea4b
Amandine4731 <5618@holbertonstudents.com>
2022-10-03 14:00:03 -0700

    learning C variables, if, else, while

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int num = '0';
-	char ch ='a';
-	
-	for (num = '0'; num <=9; num++)
+	char ch = 'a';
+
+	for (num = '0'; num <= '9'; num++)
 	putchar(num);
 
 	for (ch = 'a'; ch <= 'f'; ch++)
