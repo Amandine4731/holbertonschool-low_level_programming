42cbb49dbe173474d0e0ccc16367770d55eba0f6
Amandine4731 <5618@holbertonstudents.com>
2022-10-04 04:39:42 -0700

    learning C variables, if, else, while

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	while(num <= 9)
+	putchar(num);
+
+	putchar('\n');
+	return (0);
+}
