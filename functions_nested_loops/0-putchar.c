e68d3240a9ba43ce22a58461f2032819f5079587
Amandine4731 <5618@holbertonstudents.com>
2022-10-04 06:56:01 -0700

    learning C functions, nested, loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <main.h>
 /**
  * main - Entry point
  *
@@ -8,7 +6,7 @@
  */
 int main(void)
 {
-printf("_putchar\n")
+write("_putchar\n")
 
 return (0);
 }
