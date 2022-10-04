a72b703586b384a01b437136cbdc5f1fa847377d
Amandine4731 <5618@holbertonstudents.com>
2022-10-04 05:24:35 -0700

    learning C variables, if, else, while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,17 @@
  */
 int main(void)
 {
-	int i, n;
-	for (n = 1; n <= 10; n++) {
-	for (i = n;;) {
-	if (i / 10 % 10 > i % 10) break;
-	if ((i /= 10) == 0) { printf("%d\n", n); break; }
-}
-}
-}
+	int num = '0';
+
+	for (num = '0'; num <='9'; num++)
+		{
+		putchar(num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);		
 }
