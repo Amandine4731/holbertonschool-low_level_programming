58a7e50a69f91b7045205227dd1639a9a549d9fc
Amandine4731 <5618@holbertonstudents.com>
2022-10-05 04:41:19 -0700

    learning C functions, nested loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - main Entry point
- * Description: print _putchar in ASCII
+ * Description: print _putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 /* main - main Entry point */
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
+	char ch[] = "_putchar";
+
+	int i = 0;
+	while (ch[i] != 0)
+	{
+	_putchar(ch[i]);
+	i++;
+	}
 	_putchar('\n');
 
 	return (0);
