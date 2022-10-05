e02ff9a08e138177489e1a2b4dd6ced2da8adfc2
Amandine4731 <5618@holbertonstudents.com>
2022-10-05 05:43:16 -0700

    learning C functions, nested loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+/* prototype for the function */
+{
+	char ch = 'a';
+	int i = 0;
+	
+	while (i <= 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	i++;
+	i = 0;
+	_putchar('\n');
+	}
+}
+
