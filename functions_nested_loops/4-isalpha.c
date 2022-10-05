cae8dc1f49f6d3b81852be2c60561e9863c2e552
Amandine4731 <5618@holbertonstudents.com>
2022-10-05 09:00:17 -0700

    learning C functions, nested loops

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,21 +2,24 @@
 /**
 *_isalpha - checks for an alphabetic character
 *@c: the character to check
-* Return: 1 if c is lowercase, 0 otherwise
+* Return: 1 if c is an alphabetic character, 0 otherwise
 */
 int _isalpha(int c)
 /* prototype for the function */
 {
-	if (c >= 'a' && c <= 'z')
+	if (n > 0)
 	{
 		return (1);
+		_putchar('+');
 	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
+	else if (n == 0)
 	{
 		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
 	}
 }
---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_sign - check the sign of a number
+* @n: the character to check
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+int print_sign(int n)
+/* prototype for the function */
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
