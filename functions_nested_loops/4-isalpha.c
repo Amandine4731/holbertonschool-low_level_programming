1fd04107dfb7e5a4125d3cee9f6e00dbe73c6078
Amandine4731 <5618@holbertonstudents.com>
2022-10-05 08:36:29 -0700

    learning C functions, nested loops

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_isalpha - checks for an alphabetic character
+*@c: the character to check
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+int _isalpha(int c)
+/* prototype for the function */
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if else (c >= 'A' && c <= 'Z')
+	{
+	        return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
