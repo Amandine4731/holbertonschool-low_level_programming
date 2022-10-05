a49843e23c36442dc15ccca88b132224fd5d84bc
Amandine4731 <5618@holbertonstudents.com>
2022-10-05 08:14:22 -0700

    learning C functions, nested loops

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
+  * @c: The character to print 
   * _islower - Entry point
   * Return: Always 0.
   */
 int _islower(int c)
 /* prototype for the function */
-{	
-	if(c >= 'a' && c <= 'z')
+{
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
