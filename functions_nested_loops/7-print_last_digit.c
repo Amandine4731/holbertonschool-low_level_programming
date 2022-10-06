20e6365f6cd72ceb574aeb6cf2b48c9b72b6f305
Amandine4731 <5618@holbertonstudents.com>
2022-10-06 04:27:41 -0700

    learning C functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the value to check
+ * return: Always 0.
+ **/
+int print_last_digit(int n)
+{
+int a = -n;
+a = n % 10;
+_putchar(a + '0');
+return (0);
+}
