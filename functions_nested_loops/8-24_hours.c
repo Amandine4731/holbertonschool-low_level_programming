d3b707594495b38e41aeb72126eceef5d2b4338c
Amandine4731 <5618@holbertonstudents.com>
2022-10-06 06:47:16 -0700

    learning C functions, nested loops

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the value to check
+ * return: Always 0.
+ **/
+int print_last_digit(int n)
+{
+int lastDigit = 0; /* I initialize lastDigit */
+int l;
+lastDigit = n % 10; /* I take the last digit of the value */
+l = lastDigit; /* capture the state of between conversion */
+lastDigit = lastDigit + '0'; /* I convert the last digit to ASCII */ 
+_putchar(lastDigit); /* print the last digit */
+return (l); /* return in real number (beacause in the example, Julien convert in ASKII */
+}
+/* l = no convert; lastDigit = convert in ASCII */
