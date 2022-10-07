f47c5961aa17a83fb8cf5e445982bc4433d8ac55
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 00:53:43 -0700

    learning C more functions, nested loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_most_numbers - print numbers exept "2" and "4"
+  */
+void print_most_numbers(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+	if (n != 2 && n!=4)
+	{
+	_putchar(n);
+	n++;
+	}
+	_putchar('\n');
+
+}
