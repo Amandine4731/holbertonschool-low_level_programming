bb5b5a6e283d8c98af479172038a998efa36c497
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 03:08:03 -0700

    learning C more functions, nested loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,17 @@
   */
 void more_numbers(void)
 {
-	int i;
-	int n = '0';
-
+	int i, n;
 	for (i = 0; i < 10; i++)
 	{
-	for (n = '0'; n <= '14'; n++)
+	for (n = 0; n < 15; n++)
 	{
-	_putchar(n);
+	if (n > 9)
+	{
+	_putchar(n / 10 + '0');
+	}
+	_putchar(n % 10 + '0');
 	}
 	_putchar('\n');
-	}
 }
+} 
