a893634e043e238bf5a0d76de8b8dd7d10ba5537
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 01:22:55 -0700

    learning C more functions, nested loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * most_numbers - print numbers 10 times
+  */
+void most_numbers(void)
+{
+	int i;
+	int n = '0';
+
+	for (i = 0; i < 10; i++)
+	{
+	for (n = '0'; n <= '9'; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n')
+	}
+}
