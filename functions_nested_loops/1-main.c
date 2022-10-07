cf8a6a45983e2939d6df5aab4f470843cd593384
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 06:32:11 -0700

    learning C more functions, nested loops

--- functions_nested_loops/1-main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * main - Entry point
- */
-int main()
-{
-	print_alphabet();
-	return (0);
-}
--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,38 @@
 #include "main.h"
 /**
- * print_last_digit - print the last digit
- * @n: the value to check
+ * jack_bauer - print the last digit
  * return: Always 0.
  **/
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-int lastDigit = 0; /* I initialize lastDigit */
-int l;
-lastDigit = n % 10; /* I take the last digit of the value */
-l = lastDigit; /* capture the state of between conversion */
-lastDigit = lastDigit + '0'; /* I convert the last digit to ASCII */ 
-_putchar(lastDigit); /* print the last digit */
-return (l); /* return in real number (beacause in the example, Julien convert in ASKII */
+	  int h, m;
+	  for (h = 0; h < 24; h++) 
+		    {
+			    for (m = 0; m < 60; m++)
+				        {
+						  if (h <= 9)
+							    {
+								        putchar('0');
+									    putchar(h % 10 + '0');
+									        putchar(':');
+										  } 
+		    else
+{
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
 }
-/* l = no convert; lastDigit = convert in ASCII */
+if (m <= 9)
+{
+_putchar('0');
+_putchar(m % 10 + '0');
+} 
+else
+{
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+}
+_putchar('\n');
+}
+}
+}
