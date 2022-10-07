8df151696574dd5244d06dfd848d99798c98383d
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 11:51:45 -0700

    learning C more functions, nested loops

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,29 @@
 #include "main.h"
 /**
-  * print_to_98 - print numbers to 98
-  * @n: to check values
-  */
+ * print_to_98 - print numbers to 98
+ * @n: to check values
+ */
 void print_to_98(int n)
 {
-int i;
 
-for (n = i; n < 99; n++)
-{
-if (n > 9)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-if (n < -9)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-if (n != 98)
-{
-putchar(',');
-putchar(' ');
-}
-}
-_putchar('\n');
+	for (; n < 99; n++)
+	{
+		if (n > 9)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		if (n < -9)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
 }
 
