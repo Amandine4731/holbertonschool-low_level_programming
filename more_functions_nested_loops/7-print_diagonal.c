d6a025086c882a387c23e67eb1d02477297bc910
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 04:54:25 -0700

    learning C more functions, nested loops

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * prin_diagonal - draws a diagonal line on the terminal
+  * @n: to check
+  */
+void print_diagonal(int n)
+{
+	int r;
+	char s = '\\';
+
+	for (r = 0; r = n; r++)
+	{
+	if (n <= 0)
+	{
+	}
+	else
+	{
+	_putchar(s);
+	}
+	_putchar('\n');
+}
+}
