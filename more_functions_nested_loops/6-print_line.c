6cb5005466557efede3e0d2efac3a390f93f38af
Amandine4731 <5618@holbertonstudents.com>
2022-10-07 04:23:46 -0700

    learning C more functions, nested loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+  * print_line - print a straight line
+  * @n: to check
+  * Return:
+  */
+void print_line(int n)
+{
+int r;
+char t = '_';
+
+for (r = 0; r <= n; r++)
+{
+if (r <= 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar(t);
+_putchar('\n');
+}
+}
+}
