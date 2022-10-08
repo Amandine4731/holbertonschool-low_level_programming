d5f29adeaa8f70f315e8ee9d9feb68824c7732c5
Amandine4731 <5618@holbertonstudents.com>
2022-10-08 10:40:29 -0700

    learning C more functions, more nested loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,27 @@
 #include "main.h"
 /**
-  * print_square - print a square
-  * @size: the size of the square
-  */
+ * print_square - print a square
+ * @size: the size of the square
+ */
 void print_square(int size)
 {
-int r; /* nombre of loop turns */
-int i;
-char t = '#';
+	int r; /* nombre of loop turns */
+	int i;
+	char t = '#';
 
-for (i = 0; i < size; i++)
-{
-for (r = 0; r < size; r++)
-{
-if (size > 0)
-{
-_putchar(t);
-}
-else
-{
-}
-}
-_putchar('\n');
-}
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (r = 0; r < size; r++)
+		{
+			if (size > 0)
+			{
+				_putchar(t);
+			}
+		}
+		_putchar('\n');
+	}
 }
