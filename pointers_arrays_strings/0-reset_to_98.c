02c17cae428afc9899387719c7e996ba36e43f59
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 04:50:42 -0700

    learning C pointers,arrays and strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * reset_to_98 - a pointer with values
+  *@*n: pointer n
+  */
+void reset_to_98(int *n)
+{
+	int n;
+
+	printf("n=%d\n", n);
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <locale.h>
+int _putchar(char ch);
+void reset_to_98(int *n);
+#endif
