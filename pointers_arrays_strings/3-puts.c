438799e9bb7e0c253d40f4e6221f7deff3d11ee2
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 07:12:14 -0700

    learning C pointers,arrays and strings

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,7 +2,6 @@
 /**
  * _puts - prints a string, followed by a new line
  * @str: the character to check
- * Return: count the length of a string
  */
 void _puts(char *str)
 {
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: the character to check
+ */
+void print_rev(char *s)
+{
+	printf("%s\n", s);
+	printf("%s", strrev(s));
+}
