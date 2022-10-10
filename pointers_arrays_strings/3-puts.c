3a18a595b253b66fe089de49b91d2a080bb070b6
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 06:04:34 -0700

    learning C pointers,arrays and strings

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - return the length of a string
+ * @s: pointer s
+ * Return: count the length of a string
+ */
+void _puts(char *str)
+{
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 #include <ctype.h>
 #include <locale.h>
 #include <string.h>
+#include <stdout.h>
 int _putchar(char ch);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
