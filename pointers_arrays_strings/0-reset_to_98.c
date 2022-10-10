bfec442f1714b753ce99d01469f223e3a1d068a5
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 04:52:27 -0700

    learning C pointers,arrays and strings

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
   * reset_to_98 - a pointer with values
   *@*n: pointer n
   */
-void reset_to_98(int *n)
+void reset_to_98(int n)
 {
-	int *n;
+	int n;
 
-	printf("n=%d\n", *n);
+	printf("n=%d\n", n);
 }
