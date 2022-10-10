414d8b2085a98035bf9f02c5a137d11eea6730b4
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 05:44:25 -0700

    learning C pointers,arrays and strings

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _strlen - return the length of a string
+  * @s: pointer s
+  */
+int _strlen(char *s)
+{
+	int size;
+	int tab[];
+
+	for (*s = 0; *s < size; *s++)
+		tab[*s] = [*s] * *s;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <locale.h>
+#include <string.h>
 int _putchar(char ch);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
