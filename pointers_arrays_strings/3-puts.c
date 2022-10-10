788d4ed7fa054acebef2c999a6789b88e5d4a7e8
Amandine4731 <5618@holbertonstudents.com>
2022-10-10 06:12:28 -0700

    learning C pointers,arrays and strings

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,10 +1,19 @@
 #include "main.h"
 /**
- * _puts - return the length of a string
- * @s: pointer s
+ * _puts - prints a string, followed by a new line
+ * @str: the character to check
  * Return: count the length of a string
  */
 void _puts(char *str)
 {
+	char *str[];
+	int i = 0;
 
+	while (*str[i] != '\0')
+	{
+	putchar(*str[i]);
+	i++;
+	}
+	_putchar('\n')
+	return (0);
 }
