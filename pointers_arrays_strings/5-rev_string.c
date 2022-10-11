275e02acd529d3640a69ca548a6c4ae85af4cfb5
Amandine4731 <5618@holbertonstudents.com>
2022-10-11 04:31:21 -0700

    learning C pointers, arrays and strings

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the character to check
+ */
+void rev_string(char *s)
+{
+	int i = 0; /* index */
+
+	while (s[i] != '\0') /* count the number of characters in your string */
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+		_putchar('\n');
+}
