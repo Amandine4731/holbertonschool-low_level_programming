a959dda827ff42cb98eccb6e7f694cb6ee53bf46
Amandine4731 <5618@holbertonstudents.com>
2022-10-11 05:35:10 -0700

    learning C pointers, arrays and strings

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,15 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		_putchar(s[i]);
 	}
 		_putchar('\n');
-	if (i = i - 1 && i >= 0)
+	for (i = i - 1; i >= 0; i--)
 	{
-		i--;
 		_putchar(s[i]);
 	}
 		_putchar('\n');
