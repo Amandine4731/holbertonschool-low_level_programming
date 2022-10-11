c272cbc6a9c7a2a4c16a8d556b4a6f2aa1585beb
Amandine4731 <5618@holbertonstudents.com>
2022-10-11 13:21:20 -0700

    learning C pointers, arrays and strings

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,22 @@
 #include "main.h"
 /**
-  * print_array - print n elements of an array of integers
-  * @a: the pointer
-  * @n: a variable
-  */
+ * print_array - print n elements of an array of integers
+ * @a: the pointer
+ * @n: a variable
+ */
 void print_array(int *a, int n)
 {	
 	int i;
 
 	for (i = 0; i < n; i++) 
-	printf("%d, ", a[i]);
+	{
+		if (i < a[i])
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
 }
