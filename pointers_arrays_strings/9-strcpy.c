f642c423ec0a9143859cb3f62360e2d1689f2364
Amandine4731 <5618@holbertonstudents.com>
2022-10-12 07:06:16 -0700

    learning C pointers, arrays and strings

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,15 +3,16 @@
   * _strcpy - copy a string pointed
   * @dest: the receiver
   * @src: the giver
-  * Return: 
+  * Return: to the receiver
   */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	
+
 	for (; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 	return (dest);
 }
