65edf6a1443d105d50a8d384d780e514c09b86eb
Amandine4731 <5618@holbertonstudents.com>
2022-10-12 05:17:06 -0700

    learning C pointers, arrays and strings

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,9 +8,15 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+
+		if (i != n - 1)
+		{
+			printf (", ");
+		}
+	i++;
 	}
 	printf("\n");
 }
