be4c17dc0cfb58512624edde521d0017dc24309c
Amandine4731 <5618@holbertonstudents.com>
2022-10-13 14:10:26 -0700

    learning C more pointers, arrays and strings

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,20 @@
 #include "main.h"
 /**
-  * string_toupper - change all lowercase letters of a string to uppercase
-  * @a: the pointer
-  * @n: number of elements
-  */
-char *string_toupper(char *)
+ * string_toupper - change all lowercase letters of a string to uppercase
+ * @a: the pointer
+ * @n: number of elements
+ */
+char *string_toupper(char *a)
 {
-	int * a = &a;
-	int c = 0;
 	int i = 0;
-	
-	i = i + '0';
-	for (i = '97'; i = '122'; i++);
-	{
-	if (a[i] = 97 && a[i] = 122)
-	{
-		i++;
-		97 = 65;
-		122 = 90;
 
-	c = c + '0';
-	for (c = '65'; c = '90'; c++);
+	while (a[i] != '\0')
 	{
-		a[c] = a[i];
+	if (a[i] >= 'a' && a[i] <= 'z')
+	{
+	a[i] = a[i] - 32;
+	}
+	i++;
 	}
 	return (a);
 }
