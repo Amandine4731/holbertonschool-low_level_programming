3e0ae27b0f05773f2280ab1c7d3554e4d87d2b4d
Amandine4731 <5618@holbertonstudents.com>
2022-10-13 01:04:43 -0700

    learning C more pointers, arrays and strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strcat - add and copy strings pointed
+  * @dest: the receiver
+  * @src: the giver
+  * Return: to the receiver
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i] + dest[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
