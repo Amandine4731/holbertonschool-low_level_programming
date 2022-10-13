3ae7947d77e1b290b75666b261be2c1da81c2224
Amandine4731 <5618@holbertonstudents.com>
2022-10-13 01:43:19 -0700

    learning C more pointers, arrays and strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,14 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
+	int n = 0;
 
-	for (; dest[i] != '\0'; i++)
+	for (; src[i] != '\0'; i++)
 	{
-	dest[i] = src[i];
+	for (; dest[n] != '\0'; n++)
+	dest[n] = src[i];
 	}
-	dest[i] = '\0';
+}
+	dest[n] = '\0';
 	return (dest);
 }
