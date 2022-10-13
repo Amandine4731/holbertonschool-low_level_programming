81a4585c758e313cddbbb95cd1dc01a4c434d0f8
Amandine4731 <5618@holbertonstudents.com>
2022-10-13 14:21:53 -0700

    learning C more pointers, arrays and strings

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,8 +4,17 @@
   * @a: the pointer
   * Return: to the destination
   */
-char *cap_string(char *)
+char *cap_string(char *a)
 {
 	int i = 0;
 
-
+while (a[i] != '\0')
+	{
+	if (a[i] >= 'a' && a[i] <= 'z')
+	{
+	a[i] = a[i] - 32;
+	}
+	i++;
+	}
+	return (a);
+}
