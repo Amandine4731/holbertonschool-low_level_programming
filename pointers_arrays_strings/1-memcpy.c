ea2bc562bdb7aff75cc20b0ed3c7197d225d4ed8
Amandine4731 <5618@holbertonstudents.com>
2022-10-15 08:49:53 -0700

    learning C even more pointers, arrays and strings

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * @dest: the destination
  * @src: the source
  * @n: This is the number of bytes to be copied
+ * Return: to the destination
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
