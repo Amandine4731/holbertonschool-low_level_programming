39053aa2a881c3de6ba311d320a06496a131bb5b
Amandine4731 <5618@holbertonstudents.com>
2022-10-15 08:49:00 -0700

    learning C even more pointers, arrays and strings

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,17 +9,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (; dest[i] != '\0'; i++)
-	{
-		for (i = 0; i < n && src[i] != '\0'; i++)
+		for (i = 0; i < n; i++)
 		{
 			dest[i] = src[i];
 
 		}
-		for (; i < n; i++)
-		{
-			dest[i] = '\0';
-		}
-	}
 	return (dest);
 }
