5aca33497ec727576af647b699b13d5ad4fb67e3
Amandine4731 <5618@holbertonstudents.com>
2022-10-16 07:42:21 -0700

    learning C even more pointers, arrays and strings

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,24 @@
 #include "main.h"
 /**
-  * _strspn - get the length of a prefix substring
-  * @s: a pointer
-  * @accept: a pointer
-  */
+ * _strspn - get the length of a prefix substring
+ * @s: a pointer
+ * @accept: a pointer
+ * Return: s or 0.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-if 
-s[i] = s[0];
-
-return
+	while (*s != '\0')
+	{
+		s++;
+	}
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (*s == accept[i])
+		{
+			return (0);
+		}
+	}
+	return (i);
 }
