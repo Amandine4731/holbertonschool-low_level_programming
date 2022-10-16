738b21fd37cc9fa7a41a1695fcab2741de11563a
Amandine4731 <5618@holbertonstudents.com>
2022-10-16 08:26:59 -0700

    learning C even more pointers, arrays and strings

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,19 @@
 #include "main.h"
 /**
-  * print_chessboard - to print the chessboard
-  * @a: the pointer
-  */
+ * print_chessboard - to print the chessboard
+ * @a: the pointer
+ */
 void print_chessboard(char (*a)[8])
 {
+	{
+		int x, y;
 
+		for (x = 0; x < 8; x++)
+		{
+			for (y = 0; y < 8; y++)
+			{
+				_putchar(a[x][y]);
+			}												}
+		_putchar('\n');
+	}
 }
