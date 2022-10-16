b16718a3913876874510466320929cd89b7330d6
Amandine4731 <5618@holbertonstudents.com>
2022-10-16 08:44:26 -0700

    learning C even more pointers, arrays and strings

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,21 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
+	int x = 0;
 
-	while (*haystack != '\0' && needle[i] != '\0')
+	for (x = 0; haystack[x] != '\0'; x++)
 	{
-		haystack++;
-		i++;
+		for (i = 0; needle[i] != '\0'; i++)
+		{
+			if (haystack[x] == needle[i])
+			{
+				break;
+			}
+		}
+		if (!needle[i])
+		{
+			break;
+		}
 	}
-	if (haystack[i] == *needle)
-	{
-		return (needle);
-	}
-	else
-	{
-		return (0);
-	}
+	return (needle);
 }
