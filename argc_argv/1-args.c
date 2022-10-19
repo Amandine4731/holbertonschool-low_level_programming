c77e57859acb37d6d31bf1d0f5e2309bbf3f199e
Amandine4731 <5618@holbertonstudents.com>
2022-10-19 01:34:17 -0700

    learning C argc, argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: arguments values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	(void) argv;
+
+	return (0);
+}
+
