e247e642dcf77916ed5dad5a1c6d0e804fbf838f
Amandine4731 <5618@holbertonstudents.com>
2022-10-21 01:50:40 -0700

    learning C recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,19 +1,28 @@
 #include "main.h"
 /**
  * _sqrt_recursion - to return the natural square root of a number
+ * @counter: variable counter
+ * @number: variable n
+ * Return: power recusive
+ */
+int _power(int counter, int number)
+{
+	if (counter * counter == number)
+	{
+		return (counter);
+	}
+	else if (counter * counter < number)
+	{
+		return (_power(counter + 1, number));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - to return the natural square root of a number
  * @n: a variable
- * Return: recusive and -1 and 1
+ * Return: recusive and -1 and the square
  */
 int _sqrt_recursion(int n)
-{	
-	int m = 0;
-
-	if (n == m * m)
-	{
-		return (m);
-	}
-	else
-	{
-		return (-1);
-	}
+{
+	return (_power(1, n));
 }
