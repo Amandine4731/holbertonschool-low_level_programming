f878d6abc964f2258798b14449aa6f1d941ba65d
Amandine4731 <5618@holbertonstudents.com>
2022-10-24 06:31:25 -0700

    learning C malloc, free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	p = malloc(sizeof(char) * (size + 1));
+	p = malloc(sizeof(char) * size);
 	if (p == NULL)
 	{
 		return (NULL);
