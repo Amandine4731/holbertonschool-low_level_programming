61d0f1a727ba24daca91b96ea8dfced78c5f275c
Amandine4731 <5618@holbertonstudents.com>
2022-10-25 08:20:02 -0700

    learning C pointers, arrays and strings

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * alloc_grid - to return a pointer to a 2 dimensional array of integers
+ * @width: a variable int
+ * @height: a variable int
+ * Return: if width or height is 0 or negative, return NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p = 0;
+	int i = 0;
+	int m = 0;
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int *) * width);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * height);
+		{
+			for (m = 0; m < height; m++)
+			{
+				p[i][m] = 0;
+			}
+		}
+	}
+	return (p);
+}
