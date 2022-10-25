de281225638c935f7efc8c5138ef7d9fedf1f6fc
Amandine4731 <5618@holbertonstudents.com>
2022-10-25 09:15:03 -0700

    learning C pointers, arrays and strings

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,15 +23,20 @@ int **alloc_grid(int width, int height)
 	if (p == NULL)
 	{
 		return (NULL);
-		free(p);
 	}
 	for (i = 0; i < height; i++)
 	{
 		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
-			return (NULL);
-			free(p[i]);
+			for (; i >= 0; i--)
+			{
+				free(p[i]);
+				p[i] = NULL;
+				return (NULL);
+			}
+			free(p);
+			p = NULL;
 		}
 		{
 			for (m = 0; m < height; m++)
