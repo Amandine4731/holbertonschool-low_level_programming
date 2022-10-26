59e77718800ffb7c30d35d59c2743118cd207625
Amandine4731 <5618@holbertonstudents.com>
2022-10-26 07:05:06 -0700

    learning C more malloc, free

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * string_nconcat - to concatenate two strings
+ * @s1: a pointer char
+ * @s2: a pointer char
+ * @n: a variable
+ * Return: a new pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p = 0;
+	unsigned int i = 0;
+	unsigned int m = 0;
+	unsigned int size = 0;
+	unsigned int x = 0;
+	unsigned int y = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (m = 0; s2[m] != '\0'; m++)
+	{
+	}
+	size = i + m;
+	p = malloc(sizeof(char) * (size + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < size - m; x++)
+	{
+		p[x] = s1[x];
+	}
+	for (y = 0; y < size - i && y < n; y++)
+	{
+		p[x + y] = s2[y];
+	}
+	p[x + y] = '\0';
+	return (p);
+}
