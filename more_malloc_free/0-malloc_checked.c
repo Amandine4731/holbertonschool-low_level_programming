00126461098abec444687ba89dba911d82409c9e
Amandine4731 <5618@holbertonstudents.com>
2022-10-27 02:44:04 -0700

    learning C more malloc, free

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-  * malloc_checked - to allocate memory using malloc
-  * @b: a variable
-  */
+ * malloc_checked - to allocate memory using malloc
+ * @b: a variable
+ */
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *p = 0;
+	unsigned int *p;
 
-p = malloc(sizeof(unsigned int) * b);
+	p = malloc(b);
 	if (p == NULL)
 	{
 		exit(98);
