3712aa46ce2e9ff6ca9c3c7ea542076a78c01275
Amandine4731 <5618@holbertonstudents.com>
2022-10-31 16:05:47 -0700

    learning C structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-  * dog - meet a dog
-  */
+ * struct dog - informations about the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: informations about the dog (name, age and owner)
+ */
 struct dog
 {
 	char *name;
