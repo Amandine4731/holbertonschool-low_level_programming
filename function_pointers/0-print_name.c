c59ed88d17138e601d28acaa4ef8c26357e1c641
Amandine4731 <5618@holbertonstudents.com>
2022-11-01 08:38:36 -0700

    learning C structures, typedef

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - to print a name
+ * @name: the variable name
+ * @f: a fonction char
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+
+	if (name)
+	{
+		for (i = 0; name[i] != '\0'; i--)
+		{
+			f(&name[i]);
+		}
+}
+}
+
