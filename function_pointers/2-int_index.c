736d6944b16b3ae8734d7f3fcb80bcfd05d22136
Amandine4731 <5618@holbertonstudents.com>
2022-11-02 07:59:42 -0700

    learning C function pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,21 +4,22 @@
  * @array: a variable int (array)
  * @size: a variable int
  * @cmp: a funtion int
+ * Return: -1 , i
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (cmp == array[i])
-	{
-		for (i = 0; i < size; i++)
-		{
-		(*cmp)(array[i]);
-		}
-		return (i);
-	}
-	else
+	if (size <= 0 && array == NULL && cmp == NULL)
 	{
 		return (-1);
 	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (0);
 }
