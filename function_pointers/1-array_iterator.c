557db787c5c0d1e2edff54fde0abb88095cb12c9
Amandine4731 <5618@holbertonstudents.com>
2022-11-02 02:27:21 -0700

    learning C function pointers

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - a function given as a parameter on each element of an array
+ * @array: a variable int (array)
+ * @size: a variable size_t (array's size)
+ * @action: a funtion int
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (action == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		for (i = 0; i < (int)size; i++)
+		{
+			(*action)(array[i]);
+
+		}
+	}
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,4 +8,5 @@
 #include <string.h>
 int _putchar(char ch);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
