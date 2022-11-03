3428538e11a474688d8dbfcf87a2bbb7aa5f2a2b
Amandine4731 <5618@holbertonstudents.com>
2022-11-03 08:11:56 -0700

    learning C funtion pointers

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,50 +1,55 @@
 #include "3-calc.h"
 /**
-  * op_add - add two numbers
-  * @a: a variable int (number 1)
-  * @b: a variable int (number 2)
-  * Return: operation
-  */
+ * op_add - add two numbers
+ * @a: a variable int (number 1)
+ * @b: a variable int (number 2)
+ * Return: operation
+ */
 int op_add(int a, int b)
 {
+	if (b == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	return (a + b);
 }
 /**
-  * op_sub - substract two numbers
-  * @a: a variable int (number 1)
-  * @b: a variable int (number 2)
-  * Return: operation
-  */
+ * op_sub - substract two numbers
+ * @a: a variable int (number 1)
+ * @b: a variable int (number 2)
+ * Return: operation
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
-  * op_mul - multiply two numbers
-  * @a: a variable int (number 1)
-  * @b: a variable int (number 2)
-  * Return: operation
-  */
+ * op_mul - multiply two numbers
+ * @a: a variable int (number 1)
+ * @b: a variable int (number 2)
+ * Return: operation
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
-  * op_div - divise two numbers
-  * @a: a variable int (number 1)
-  * @b: a variable int (number 2)
-  * Return: operation
-  */
+ * op_div - divise two numbers
+ * @a: a variable int (number 1)
+ * @b: a variable int (number 2)
+ * Return: operation
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
-  * op_mod - modulo two numbers
-  * @a: a variable int (number 1)
-  * @b: a variable int (number 2)
-  * Return: operation
-  */
+ * op_mod - modulo two numbers
+ * @a: a variable int (number 1)
+ * @b: a variable int (number 2)
+ * Return: operation
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
