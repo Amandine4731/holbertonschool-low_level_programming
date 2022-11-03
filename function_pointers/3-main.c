38856d7081fbe2857718d2ad24e5a46884fb8f2f
Amandine4731 <5618@holbertonstudents.com>
2022-11-03 07:50:48 -0700

    learning C funtion pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: name of arguments
+ * Return: the funtion
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*f)(int, int);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("error\n");
+		exit(98);
+	}
+	if (f == NULL || argv[2][1] != '\0')
+	{
+		printf("error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("error\n");
+		exit(100);
+	}
+	printf("%d\n", f(a, b));
+	return (0);
+}
+
