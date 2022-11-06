bdf6fba659b6a7850b544611f2c29768749145a2
Amandine4731 <5618@holbertonstudents.com>
2022-11-06 07:49:29 -0800

    learning C variadic functions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,17 +10,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *string;
 	va_list args;
-
+	
+	if (separator == NULL)
+		{
+			separator = "";
+		}
+	
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(args, char *);
 
-		if (separator == NULL)
-		{
-			separator = "";
-		}
-		else if (string == NULL)
+		if (string == NULL)
 		{
 			printf("(nil)");
 		}
