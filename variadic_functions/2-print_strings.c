a88ac10c4b404a616afe1e13788d6da6b04f30b0
Amandine4731 <5618@holbertonstudents.com>
2022-11-06 07:47:59 -0800

    learning C variadic functions

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - to print a string
+ * @separator: a pointer const char
+ * @n: a variable const unsigned int
+ * Return: a string
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *string;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+
+		if (separator == NULL)
+		{
+			separator = "";
+		}
+		else if (string == NULL)
+		{
+			printf("(nil)");
+		}
+		else if (i == n - 1)
+		{
+			printf("%s", string);
+		}
+		else
+		{
+			printf("%s%s", string, separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
