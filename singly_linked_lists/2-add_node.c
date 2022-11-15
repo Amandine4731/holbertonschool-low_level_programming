63020c7937f40185aa46b837c2ad19c32bb906eb
Amandine4731 <5618@holbertonstudents.com>
2022-11-15 05:25:17 -0800

    learning C singly linked list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,8 +1,26 @@
 #include "lists.h"
 /**
-  * add_none - to add a new node at the beginning of a list
-  * @head: the first node
-  * @str: a pointer string
-  * Return: 
-  */
+ * add_none - to add a new node at the beginning of a list
+ * @head: the first node
+ * @str: a pointer string
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *p;
+
+	p = malloc(sizeof(list_t));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	p->str = strdup(str);
+	p->len = strlen(str);
+	p->next = *head;
+	*head = p;
+
+	return (p);
+}
+
 
