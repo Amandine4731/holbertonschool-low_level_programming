687e7c51d97218add8d8cfea07ac60953578646d
Amandine4731 <5618@holbertonstudents.com>
2022-11-16 03:15:09 -0800

    learning C singly linked lists

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,16 +7,8 @@ void free_list(list_t *head)
 {
 	if (head != NULL)
 	{
-		free(head);
-		head = NULL;
-	}
-	else
-	{
 		free_list(head->next);
-		head->next = NULL;
 		free(head->str);
-		head->str = NULL;
 		free(head);
-		head = NULL;
 	}
 }
