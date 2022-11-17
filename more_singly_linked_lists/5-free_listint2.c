db852ae13048f4a298716d548986365dc0742a7f
Amandine4731 <5618@holbertonstudents.com>
2022-11-17 06:51:50 -0800

    learning C more singly linked list

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *copyNode;
 
-	while ((*head) != NULL)
+	if ((*head) != NULL)
 	{
-		copyNode = (*head)->next;
-		free(*head);
-		(*head) = NULL;
-		(*head) = copyNode;
+
+		while ((*head) != NULL)
+		{
+			copyNode = (*head)->next;
+			free(*head);
+			(*head) = NULL;
+			(*head) = copyNode;
+		}
 	}
 }
