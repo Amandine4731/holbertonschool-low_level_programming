1212e26067cbd1eb88c138f194a05928b1b20bb3
Amandine4731 <5618@holbertonstudents.com>
2022-11-17 12:42:40 -0800

    learning C more singly linked lists

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 /**
-  * pop_listint - eletes the head node of a list / return the head node’s data
-  * @head: pointer pointer
-  * Return: n or  if list is empty
-  */
+ * pop_listint - eletes the head node of a list / return the head node’s data
+ * @head: pointer pointer
+ * Return: n or  if list is empty
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int n = (*head)->n;
-	if (head == NULL)
-		return (0);
 
+	if ((*head) == NULL)
+	{
+		return (0);
+	}
 	else
 	{
 		temp = (*head);
