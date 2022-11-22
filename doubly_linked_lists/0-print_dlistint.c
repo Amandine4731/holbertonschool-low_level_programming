1012eb76487aa8b8a4dc376cf38b3e751febf666
Amandine4731 <5618@holbertonstudents.com>
2022-11-22 08:23:24 -0800

    learning C doubly linked lists

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - to print all the elements of a dlistint_t list
+ * @h: pointer struct
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
