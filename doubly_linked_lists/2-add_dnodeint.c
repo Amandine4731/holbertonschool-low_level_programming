0b282d75d8c9a79f8c97d84664c37585263c49d5
Amandine4731 <5618@holbertonstudents.com>
2022-11-22 09:31:16 -0800

    learning C doubly linked lists

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - to add a new node at the end of a list
+ * add_dnodeint - to add a new node at the end of a list
  * @head: the first node
  * @n: number
  * Return: the address of the new element, or NULL if it failed
