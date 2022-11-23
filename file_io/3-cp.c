38d3d42d063f0c946ba75a8afaae91a6d9faabe3
Amandine4731 <5618@holbertonstudents.com>
2022-11-23 02:59:39 -0800

    learning C File I/O

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,7 +2,7 @@
 /**
  * main - copy the content of a file to another file
  * @argc: arguments counter
- * @argv[]: arguments
+ * @argv: arguments
  * Return: copy or error
  */
 int main(int argc, char *argv[])
@@ -11,20 +11,18 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-	{
-	dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+	{	dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
 	exit(97);
 	}
 	fdsource = open(argv[1], O_RDONLY);
 	if (fdsource == -1)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
 	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while (size > 0)
 	{
-		size = read(fdsource, buffer, 1024);
+		size = read(fdsource, buffer, 1024);/* copy of fdsource in the buffer */
 		if (size == -1)
 		{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -38,13 +36,11 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (close(fdsource) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdsource);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdsource);
 		exit(100);
 	}
 	if (close(fddest) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddest);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fddest);
 		exit(100);
 	}
 	return (0);
