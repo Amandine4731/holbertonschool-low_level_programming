08b4b914f193812db61e7647b1db032d1fcbc279
Amandine4731 <5618@holbertonstudents.com>
2022-11-23 03:03:25 -0800

    learning C File I/O

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-	{	dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	exit(97);
 	}
 	fdsource = open(argv[1], O_RDONLY);
