909f5971b25b76c9c6266c2c1836ffbda1c62abf
Amandine4731 <5618@holbertonstudents.com>
2022-11-24 11:34:28 -0800

    learning C hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0 || new_table == NULL)
 		return (NULL);
 
-	new_table->array = malloc(sizeof(hash_node_t*) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	new_table->size = size;
 	return (new_table);
 }
