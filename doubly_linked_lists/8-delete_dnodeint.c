64f41c05ee604756007355fb3d236a2cfb8d160d
Amandine4731 <5618@holbertonstudents.com>
2022-11-24 09:26:27 -0800

    learning C doubly linked lists

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *p;
 
-	if (*head == NULL || index == 0)
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 		return (-1);
 
 	if ((*head)->n == (int)index)
