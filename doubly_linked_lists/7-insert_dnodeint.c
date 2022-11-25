c7cff256c4b0ad81b8dbbad5872acbb535927fb0
Amandine4731 <5618@holbertonstudents.com>
2022-11-25 01:04:46 -0800

    learning C doubly linked lists

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
-  * insert_dnodeint_at_index - to insert a new node at a given position
-  * @h: pointer pointer struct
-  * @idx: the index of the node start at 0
-  * @n: numbers
-  * Return: the adress of the new node
-  */
+ * insert_dnodeint_at_index - to insert a new node at a given position
+ * @h: pointer pointer struct
+ * @idx: the index of the node start at 0
+ * @n: numbers
+ * Return: the adress of the new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	while ((*h) != NULL)
+	while ((*h))
 	{
-		(*h) = (*h)->next;
 		if (n == (int)idx)
 			return ((*h));
+		(*h) = (*h)->next;
 		n++;
 	}
 	return (NULL);
