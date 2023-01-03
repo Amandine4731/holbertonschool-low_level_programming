d23a68cb66d087ef526b80cae41e712884113d8b
Amandine4731 <5618@holbertonstudents.com>
2023-01-03 07:07:21 -0800

    files created

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+  * linear_search - to search a value in an array with linear search algorithm
+  * @array: pointer to array
+  * @value: values in array
+  * @size: size of array
+  * Return: an int
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	(void)*array;
+	(void)size;
+	(void)value;
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+* binary_search - to search for a value in sorted array with Binary search algo
+* @array: pointer to array
+* @size: size of array
+* @value: value of the array
+* Return: an int
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	(void)*array;
+	(void)size;
+	(void)value;
+}
