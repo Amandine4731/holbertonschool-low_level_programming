64b885ee8dbf91379e41fb058dc8aac1ba242e05
Amandine4731 <5618@holbertonstudents.com>
2023-01-04 01:42:37 -0800

    test validity of task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,16 +1,22 @@
 #include "search_algos.h"
 /**
-  * linear_search - to search a value in an array with linear search algorithm
-  * @array: pointer to array
-  * @value: values in array
-  * @size: size of array
-  * Return: an int
-  */
+ * linear_search - to search a value in an array with linear search algorithm
+ * @array: pointer to array
+ * @value: values in array
+ * @size: size of array
+ * Return: an int
+ */
 int linear_search(int *array, size_t size, int value)
 {
-	(void)*array;
-	(void)size;
-	(void)value;
+	size_t i = 0; /* index of the array */
 
-	return (0);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
