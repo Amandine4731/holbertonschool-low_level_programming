dfb98113ae8894d5535b287a7739c5e0f2b0a6a4
Amandine4731 <5618@holbertonstudents.com>
2023-01-04 08:09:52 -0800

    test validity task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -31,7 +31,7 @@ int binary_search(int *array, size_t size, int value)
 			for (i = first; i < last; i++)
 				printf("%d, ", array[i]);
 			printf("%d\n", array[last]);
-			continue;
+			return (i);
 		}
 		else
 		{
