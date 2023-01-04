fce8cdce3a1a6a3326f64a6c68913cd59e23800c
Amandine4731 <5618@holbertonstudents.com>
2023-01-04 07:54:26 -0800

    test validity task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * binary_search - to search for a value in sorted array with Binary search algo
+ * binary_search - to search for a value in sorted array with Binary search
  * @array: pointer to array
  * @size: size of array
  * @value: value of the array
@@ -8,32 +8,28 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t first = 0; /* first element of the array */
-	size_t last = (size - 1); /* last element of the array */
-	size_t i = 0;
+	size_t first = 0, last = (size - 1), i = 0;
 
+	if (size <= 0 && array == NULL)
+		return (-1);
 	printf("Searching in array: ");
 	for (i = 0; i < last; i++)
-	{
 		printf("%d, ", array[i]);
-	}
 	printf("%d\n", array[last]);
 	while (first < last)
 	{
-		size_t middle = (first + (last - first) / 2); /* the middle of the array*/
+		size_t middle = (first + (last - first) / 2);
+
 		if (value == array[middle])
 		{
 			return (middle);
-			break;
 		}
 		else if (value < array[middle])
 		{
 			last = middle - 1;
 			printf("Searching in array: ");
 			for (i = first; i < last; i++)
-			{
 				printf("%d, ", array[i]);
-			}
 			printf("%d\n", array[last]);
 			continue;
 		}
@@ -42,9 +38,7 @@ int binary_search(int *array, size_t size, int value)
 			first = middle + 1;
 			printf("Searching in array: ");
 			for (i = first; i < last; i++)
-			{
 				printf("%d, ", array[i]);
-			}
 			printf("%d\n", array[last]);
 			continue;
 		}
